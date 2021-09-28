0e09afc2d6cc88f209cce675483ab14b29b9140d
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-28 16:16:25 +0300

    debug the login and signup files

--- app/auth.py
+++ app/auth.py
@@ -4,9 +4,7 @@ from .models import User
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 
-
 auth = Blueprint('auth', __name__)
-
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -24,16 +22,13 @@ def login():
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
-
     return render_template("login.html", user=current_user)
-
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
-
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -51,7 +46,7 @@ def sign_up():
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
+            flash('Passwords missmatch.', category='error')
         elif len(password1) < 4:
             flash('Password must be at least 4 characters.', category='error')
         else:
